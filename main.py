449f39e9e15ad6c65e89c514e950d19ec9fbe550
ChocoTonic <nutspizza@gmail.com>
2025-03-16 00:19:55 -0700

    Implement main script for OtterAI integration

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+from pprint import pprint
+
+from dotenv import load_dotenv
+
+from otterai.otterai import OtterAI, OtterAIException
+
+load_dotenv()
+
+def main():
+    try:
+        otter = OtterAI()
+
+        username =os.getenv("OTTER_USERNAME")
+        print(f"Username: {username}")
+        password = os.getenv("OTTER_PASSWORD")
+        print(f"Password: ...{password[-4:]}")
+
+        login_response = otter.login(username, password)
+        print(f"Login successful: {login_response['status'] == 200}")
+
+        user_info = otter.get_user()
+        # print("User info:")
+        # pprint(user_info)
+
+        speeches = otter.get_speeches()
+        print("Speeches:")
+        pprint(speeches)
+    except OtterAIException as e:
+        print(f"An error occurred: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+if __name__ == "__main__":
+    main()
