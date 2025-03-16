85d2b76f0dac07e079252cc6b272e159577e041c
ChocoTonic <nutspizza@gmail.com>
2025-03-16 14:25:09 -0700

    Fix formatting inconsistencies in main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from otterai.otterai import OtterAI, OtterAIException
 
 load_dotenv()
 
+
 def main():
     try:
         otter = OtterAI()
 
-        username =os.getenv("OTTERAI_USERNAME")
+        username = os.getenv("OTTERAI_USERNAME")
         print(f"Username: {username}")
         password = os.getenv("OTTERAI_PASSWORD")
         print(f"Password: ...{password[-4:]}")
@@ -25,11 +26,13 @@ def main():
 
         speeches = otter.get_speeches()
         # print("Speeches:")
+
         # pprint(speeches)
     except OtterAIException as e:
         print(f"An error occurred: {e}")
     except Exception as e:
         print(f"Unexpected error: {e}")
+
 
 if __name__ == "__main__":
     main()
